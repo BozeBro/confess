54ca5ff16a45addb458bc783a92c6d0de32b987e
BozeBro <ben.ozua@gmail.com>
2022-09-27 17:28:40 -0400

    Created image generator

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
-# Cardo, bold, #8b0203
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
+from image_utils import ImageText
+from cleantext import clean
 
-w, h = 220, 190
-shape = [(0, 372), (1080, 707)]
-with Image.open("confess.png") as im:
-    draw = ImageDraw.Draw(im) 
-    draw.rectangle(shape, fill="white")
+# Cardo, bold, #8b0203
+# returns a PIL Image
+def makeIMG(text):
+    w = 1080
+    ORIGIN = (0, 0)
+    WHITE  = (255, 255, 255)
+    heightEnd      = 707
+    heightBeg      = 340
+    diff           = heightEnd - heightBeg
+    color          = (139, 2, 3)
+    font           = "Cardo-Bold.ttf"
+    background     = "confess.png"
+    height         = -float("inf")
+    place          = "center"
+    position       ='middle'
+    text = clean(text.strip(), no_emoji=True).strip()
+    left = 0
+    right = 400
+    size = 200
+    params = {
+        "xy" : ORIGIN,
+        "text" : text, 
+        "box_width" : w,
+        "font_filename" : font, 
+        "font_size" : left,
+        "color" : color,
+        "place" : place,
+    }
+
+    while left < size < right :
+        im = ImageText((w, diff), background=WHITE)
+        _, height = im.write_text_box(
+            ORIGIN, text, box_width=w, font_filename=font, 
+            font_size=size, color=color, place=place)
+        if height < diff:
+            left = size
+        else:
+            right = size 
+        size = (left + right) // 2
+
+    im = ImageText((w, diff), background=WHITE)
+    im.write_text_box(
+        ORIGIN, text, box_width=w, font_filename=font, 
+        font_size=size, color=color, place=place, position=position)
     im.show()
+    quit()
+    #background = Image.open(background)
+    #background.paste(im.getImage(), (0, heightBeg), mask=im.getImage())
+    return background
+
+if __name__ == "__main__":
+    img = makeIMG("123 💬")
+    img.show()
