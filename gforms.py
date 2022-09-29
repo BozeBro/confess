74a460c2f4a64d456e0ab9ab6b00f376827a1cc5
BozeBro <ben.ozua@gmail.com>
2022-09-29 14:36:28 -0400

    Reading forms and creating images

--- gforms.py
+++ gforms.py
@@ -2,5 +2,40 @@
 LINK: https://docs.google.com/forms/d/e/1FAIpQLSe_fRARQb7S8Q5Y0O86vgaGueYNxQRGbZwbTa3keSR_fyMk8w/viewform?usp=sf_link
 
 """
-LINK = r"https://docs.google.com/forms/d/e/1FAIpQLSe_fRARQb7S8Q5Y0O86vgaGueYNxQRGbZwbTa3keSR_fyMk8w/viewform?usp=sf_link"
+from __future__ import print_function
+import json
+from apiclient.discovery import build
+from google.oauth2 import service_account
+from post import makeManyIMG
+SCOPES = ["https://www.googleapis.com/auth/forms.responses.readonly", "https://www.googleapis.com/auth/forms.currentonly", "https://www.googleapis.com/auth/forms"]
+FORM_ID = r"1wSbJXHo-a3VgWktVMTrQhA3a1E_bGRHq5vsuoeMw24w"
 
+def getResponses(my_form_id : str):
+    credentials = service_account.Credentials.from_service_account_file(
+    './service_secret.json')
+    scoped_creds = credentials.with_scopes(SCOPES)
+    with build('forms', 'v1', credentials=scoped_creds) as service:
+        forms = service.forms()
+        responses = forms.responses().list(formId=my_form_id).execute()
+    return responses
+def foo():
+    # example of how to read json format
+    with open("example.json", "r") as file:
+        obj = json.load(file)
+        submissions = []
+        for resp in obj["responses"]:
+            my_id = list(resp["answers"].keys())[0]
+            submission = resp["answers"][my_id]["textAnswers"]["answers"][0]["value"]
+            submissions.append(submission)
+        makeManyIMG(submissions)
+def transformRes(responses):
+    submissions = []
+    for resp in responses["responses"]:
+        my_id = list(resp["answers"].keys())[0]
+        submission = resp["answers"][my_id]["textAnswers"]["answers"][0]["value"]
+        submissions.append(submission)
+    makeManyIMG(submissions)
+
+if __name__ == "__main__":
+    resps = getResponses(FORM_ID)
+    transformRes(resps)
--- post.py
+++ post.py
@@ -1,11 +1,12 @@
 from PIL import Image
 from image_utils import ImageText
 from cleantext import clean
-
+from typing import List
+from math import floor
 # Cardo, bold, #8b0203
 # returns a PIL Image
 # Form link 
-def makeIMG(text):
+def makeIMG(text : str) -> Image:
     w = 1080
     ORIGIN = (0, 0)
     WHITE  = (255, 255, 255)
@@ -19,19 +20,10 @@ def makeIMG(text):
     place          = "center"
     position       ='middle'
     text = clean(text.strip(), no_emoji=True).strip()
+
     left = 0
     right = 400
     size = 200
-    params = {
-        "xy" : ORIGIN,
-        "text" : text, 
-        "box_width" : w,
-        "font_filename" : font, 
-        "font_size" : left,
-        "color" : color,
-        "place" : place,
-    }
-
     while left < size < right :
         im = ImageText((w, diff), background=WHITE)
         _, height = im.write_text_box(
@@ -50,7 +42,13 @@ def makeIMG(text):
     background = Image.open(background)
     background.paste(im.getImage(), (0, heightBeg), mask=im.getImage())
     return background
-
+def makeManyIMG(texts : List) -> int:
+    counter = 0
+    imgGenerator = map(makeIMG, filter(lambda x : x.strip() != "", texts))
+    for img in imgGenerator:
+        img.save(f"./confessions/img{counter}.png")
+        counter += 1
+    return counter
 if __name__ == "__main__":
     tes = r'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
     img = makeIMG(tes)
