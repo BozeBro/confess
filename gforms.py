d16aef2083677284569bde011d8f9439aafc6f30
BozeBro <ben.ozua@gmail.com>
2022-09-29 00:05:53 -0400

    starting gforms file

---
+++ gforms.py
@@ -0,0 +1,6 @@
+"""
+LINK: https://docs.google.com/forms/d/e/1FAIpQLSe_fRARQb7S8Q5Y0O86vgaGueYNxQRGbZwbTa3keSR_fyMk8w/viewform?usp=sf_link
+
+"""
+LINK = r"https://docs.google.com/forms/d/e/1FAIpQLSe_fRARQb7S8Q5Y0O86vgaGueYNxQRGbZwbTa3keSR_fyMk8w/viewform?usp=sf_link"
+
