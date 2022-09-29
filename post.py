f27fa8a592f82a9313f0d2c4e4eb4524cfe5b927
BozeBro <ben.ozua@gmail.com>
2022-09-29 00:05:45 -0400

    renaming files

--- post.py
+++ post.py
@@ -4,6 +4,7 @@ from cleantext import clean
 
 # Cardo, bold, #8b0203
 # returns a PIL Image
+# Form link 
 def makeIMG(text):
     w = 1080
     ORIGIN = (0, 0)
@@ -46,12 +47,11 @@ def makeIMG(text):
     im.write_text_box(
         ORIGIN, text, box_width=w, font_filename=font, 
         font_size=size, color=color, place=place, position=position)
-    im.show()
-    quit()
-    #background = Image.open(background)
-    #background.paste(im.getImage(), (0, heightBeg), mask=im.getImage())
+    background = Image.open(background)
+    background.paste(im.getImage(), (0, heightBeg), mask=im.getImage())
     return background
 
 if __name__ == "__main__":
-    img = makeIMG("123 💬")
+    tes = r'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+    img = makeIMG(tes)
     img.show()
