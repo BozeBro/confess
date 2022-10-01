b98da5443222cf377db43bfc6d53ba2635c7882b
Benedict Ozua <ben.ozua@gmail.com>
2022-10-01 16:11:51 -0400

    addedf google drive file

---
+++ drive.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from PIL import Image
+from cliforms import getcreds
+
+
+def uploadImg(folder_id, img, img_name, scopes):
+    creds = getcreds(scopes)
+    service = build("drive", "v3", credentials=creds)
+
+    file_metadata = {"name": img_name, "parents": [folder_id]}
+    media = MediaFileUpload(
+        "./confessions/" + img_name, mimetype="image/png", resumable=True
+    )
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+
+
+def uploadImgs(folder_id, imgs, img_names, scopes):
+    for img, name in zip(imgs, img_names):
+        uploadImg(folder_id, img, name, scopes)
+
+
+if __name__ == "__main__":
+    scopes = ["https://www.googleapis.com/auth/drive"]
+    img = Image.open("./confessions/img0.png")
+    data = img.getdata()
+    name = "img0.png"
+    uploadImg(folder_id, data, name, scopes)
