78cf2f7a5fc7f27bafd6148c6521c2b30bdfd408
Benedict Ozua <ben.ozua@gmail.com>
2022-10-01 16:09:06 -0400

    fixed counter ordering

--- post.py
+++ post.py
@@ -49,12 +49,12 @@ def saveImgs(imgs):
         val = f.read().strip()
         counter = 0
         if val.isnumeric():
-            counter = int(val) + 1
+            counter = int(val)
     for img in imgs:
         img.save(f"./confessions/img{counter}.png")
         counter += 1
     with open("counter.txt", "w") as f:
-        f.write(str(counter - 1))
+        f.write(str(counter))
 if __name__ == "__main__":
     tes = r'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
     img = makeIMG(tes)
