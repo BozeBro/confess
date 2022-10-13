2c6d1100578555c5609a01ae35077b455e882d41
BozeBro <ben.ozua@gmail.com>
2022-10-13 18:03:24 -0400

    remove uneeded function

--- google_util/database.py
+++ google_util/database.py
@@ -2,11 +2,6 @@ from unicodedata import name
 from google.cloud import datastore
 from typing import List
 datastore_client = datastore.Client()
-def makeNames(n: int) -> List[str]:
-    key = datastore_client.key('tracker', 'counter')
-    counter = datastore_client.get(key)
-    names = [f"img{i+counter['count']}.png" for i in range(n)]
-    return names
 
 def saveImgs(imgs, names):
     datastore_client = datastore.Client()
