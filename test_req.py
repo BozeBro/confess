ffb5fc3f0c8a0275bceb36dd46e1a5fdce51385d
BozeBro <ben.ozua@gmail.com>
2022-10-15 21:32:47 -0400

    Test for sending requests

--- test_req.py
+++ test_req.py
@@ -3,6 +3,8 @@ import requests
 # http://127.0.0.1:8080
 # https://marine-alchemy-363921.uk.r.appspot.com/get_image
 if __name__ == "__main__":
+    localhost = "http://127.0.0.1:8080/test"
+    prod = "https://marine-alchemy-363921.uk.r.appspot.com"
     data = {"confessions": ["I like peanuts", "YOLO", "LOREM IPSUM IS THE BEST CHEESE"]}
-    r = requests.post("http://127.0.0.1:8080/get_image", json=data)
-    print(r.json())
+    r = requests.post(localhost, json=data)
+    print(r.content)
